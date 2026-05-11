d84d76a579a344393d465a992732da83121cbe09
camch-prog <charassoncamille@gmail.com>
2026-05-11 14:24:29 +0200

    create file

---
+++ exo JS/main.js
@@ -0,0 +1,12 @@
+
+let chaine = "une chaine de caractère";
+let nombre=12;
+let nombre_virgule =1.2;
+let tableau =["aa","ok"];
+let objet = {
+    nom:"Bob",
+    age : 15
+};
+let truc = function() {
+    console.log("Hello world");
+}; 
