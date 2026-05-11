30165011b877356c4aa6c2ea170a4b79e87b81b1
camch-prog <charassoncamille@gmail.com>
2026-05-11 16:45:13 +0200

    modified:   exo JS/exercice calcul/main.js

--- exo JS/exercice calcul/main.js
+++ exo JS/exercice calcul/main.js
@@ -4,8 +4,8 @@ console.log(calcul)
 let unCompteur = 0;
 unCompteur += 5;
 unCompteur --;
-unCompteur *3;
-unCompteur/2;
+unCompteur *= 3;
+unCompteur /= 2;
 console.log(unCompteur)
 
 console.log(10*41+(1-4)/78)
