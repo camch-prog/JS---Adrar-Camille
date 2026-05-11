57efe763549a20d33c6f7422d5382d3bf68b7f70
camch-prog <charassoncamille@gmail.com>
2026-05-11 15:59:57 +0200

    renamed:    exo JS/index.html -> exo JS/exercice variable/index.html 	renamed:    exo JS/main.js -> exo JS/exercice variable/main.js 	new file:   exo JS/exercice/index.html 	new file:   exo JS/exercice/main.js

---
+++ exo JS/exercice/main.js
@@ -0,0 +1,11 @@
+let calcul = 1+2*8/7
+console.log(calcul)
+
+let unCompteur = 0;
+unCompteur += 5;
+unCompteur --;
+unCompteur *3;
+unCompteur/2;
+console.log(unCompteur)
+
+console.log(10*41+(1-4)/78)
