1fd9c92dc1a3b2fd4381df9cce017fd85dec3b1e
camch-prog <charassoncamille@gmail.com>
2026-05-12 11:09:08 +0200

    modified:   exo JS/exercice pizzeria/pizzeria.js

--- exo JS/exercice pizzeria/pizzeria.js
+++ exo JS/exercice pizzeria/pizzeria.js
@@ -1,15 +1,20 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 let dateDuJour = "12/05/2026"
 let nomClient = "Bernard";
 let adresseClient = "5 rue des Lilas";
 let villeClient = "Toulouse";
-let pizzaCommandee= " Margarita";
+let listePizza = ["Margarita", "Pepperoni", "Suprême","Reine"]
+let pizzaCommandee= listePizza[(getRandomInt(4))];
 let tempsLivraison = 30;
 let messageSuprise = `Qu'est qu'une pizza dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`
 
 
 let SumUpOrderPhrase = `${dateDuJour}:\n
 Bonjour, ${nomClient} ,votre pizza ${pizzaCommandee} est en cours de préparation.\n
-Elle vous sera livrée d'ici ${tempsLivraison}minutes à l'adresse : ${adresseClient}, ${villeClient}.\n
+Elle vous sera livrée d'ici ${tempsLivraison} minutes à l'adresse : ${adresseClient}, ${villeClient}.\n
 ---\n
 ${messageSuprise}`;
 
