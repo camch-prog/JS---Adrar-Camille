1b64b2fa71e7f73b2643e83db8f3f6d6e3a0e3b6
camch-prog <charassoncamille@gmail.com>
2026-05-12 10:58:40 +0200

    modified:   exo JS/exercice pizzeria/pizzeria.js

--- exo JS/exercice pizzeria/pizzeria.js
+++ exo JS/exercice pizzeria/pizzeria.js
@@ -2,7 +2,6 @@ let dateDuJour = "12/05/2026"
 let nomClient = "Bernard";
 let adresseClient = "5 rue des Lilas";
 let villeClient = "Toulouse";
-let sexeH = true;
 let pizzaCommandee= " Margarita";
 let tempsLivraison = 30;
 let messageSuprise = `Qu'est qu'une pizza dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`
