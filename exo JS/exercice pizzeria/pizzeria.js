495a0f5d97c4620a30173024b88532c95c48a6bd
camch-prog <charassoncamille@gmail.com>
2026-05-12 10:58:17 +0200

    new file:   exo JS/exercice pizzeria/pizzeria.html 	new file:   exo JS/exercice pizzeria/pizzeria.js 	modified:   exo JS/exercice pizzeria/pizzeria.js

---
+++ exo JS/exercice pizzeria/pizzeria.js
@@ -0,0 +1,18 @@
+let dateDuJour = "12/05/2026"
+let nomClient = "Bernard";
+let adresseClient = "5 rue des Lilas";
+let villeClient = "Toulouse";
+let sexeH = true;
+let pizzaCommandee= " Margarita";
+let tempsLivraison = 30;
+let messageSuprise = `Qu'est qu'une pizza dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`
+
+
+let SumUpOrderPhrase = `${dateDuJour}:\n
+Bonjour, ${nomClient} ,votre pizza ${pizzaCommandee} est en cours de préparation.\n
+Elle vous sera livrée d'ici ${tempsLivraison}minutes à l'adresse : ${adresseClient}, ${villeClient}.\n
+---\n
+${messageSuprise}`;
+
+console.log(SumUpOrderPhrase);
+
