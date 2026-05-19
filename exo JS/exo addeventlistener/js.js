86b0d9681caa54b552db2f4d6ecb4acffa3caa1a
camch-prog <charassoncamille@gmail.com>
2026-05-19 16:45:06 +0200

    modified:   exo JS/exo addeventlistener/js.js

--- exo JS/exo addeventlistener/js.js
+++ exo JS/exo addeventlistener/js.js
@@ -1,4 +1,12 @@
 let titre = document.getElementsByTagName("h1")[0];
+let normal = false
 titre.addEventListener("click",()=>{
+    if (normal === true){
     titre.innerText = ":)"
+    normal =false
+    }
+    else{
+        titre.innerText = "a"
+        normal = true
+    }
 })
