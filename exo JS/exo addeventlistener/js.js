2c18a0cb4182bf7cbcae8da34c1185371bbf088a
camch-prog <charassoncamille@gmail.com>
2026-05-20 09:50:56 +0200

    new file:   exo JS/exo addeventlistener/css.css

--- exo JS/exo addeventlistener/js.js
+++ exo JS/exo addeventlistener/js.js
@@ -1,12 +1,27 @@
 let titre = document.getElementsByTagName("h1")[0];
 let normal = false
 titre.addEventListener("click",()=>{
-    if (normal === true){
-    titre.innerText = ":)"
-    normal =false
-    }
-    else{
-        titre.innerText = "a"
-        normal = true
-    }
+    // if (normal === true){
+    // titre.innerText = ":)"
+    // normal =false
+    // }
+    // else{
+    //     titre.innerText = "a"
+    // normal = true
+    // }
+    titre.innerText= normal ? ':)' : "a";
+    normal=!normal;
 })
+
+let truefalse = false
+
+let dark = document.getElementById("dark")
+let suppr = document.getElementById("suppr")
+let toggle = document.getElementById('toggle')
+dark.addEventListener("click",()=>{
+    titre.classList='dark'})
+suppr.addEventListener("click",()=>{
+    titre.classList.remove('dark')})
+toggle.addEventListener("click",()=>{
+    titre.classList= truefalse ? 'dark' : "";
+    truefalse=!truefalse;})
