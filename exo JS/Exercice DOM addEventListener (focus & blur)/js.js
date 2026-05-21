9b0b5eb88d4614b6c1a84637d5866c4379ce55a7
camch-prog <charassoncamille@gmail.com>
2026-05-21 16:58:33 +0200

    modified:   exo JS/Exercice DOM addEventListener (focus & blur)/html.html 	modified:   exo JS/Exercice DOM addEventListener (focus & blur)/js.js 	new file:   exo JS/Exercice DOM addEventListener (focus & blur)/style.css 	modified:   exo JS/exercice perso/index.html

--- exo JS/Exercice DOM addEventListener (focus & blur)/js.js
+++ exo JS/Exercice DOM addEventListener (focus & blur)/js.js
@@ -9,3 +9,17 @@ input.addEventListener("blur", () => {
     input.style.background = "inherit";
     input.style.color ="inherit";
 }); 
+let h1 = document.getElementsByTagName("h1")[0];
+let toutLeDoc = document;
+toutLeDoc.addEventListener('mouseleave',()=>{
+    h1.style.display = "block";
+    h1.style.background='chartreuse';
+    h1.style.color='red';
+    h1.style.textAlign= 'center';
+})
+
+let field=document.getElementById("input-exercice-keyup");
+let ecriteau = document.getElementById("renderKeyboard")
+field.addEventListener("keyup", ()=>{
+    
+})
